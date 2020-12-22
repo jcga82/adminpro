baf24a3c4a9ca13622dbc884b7a013d0f34a57d1
Juan Carlos <jcga82@gmail.com>
2020-12-22 21:34:29 +0100

    Actualizado a Angular 11

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+//Módulos
+import { PagesModule } from './pages/pages.module';
+
+//Componentes
+import { AppComponent } from './app.component';
+import { LoginComponent } from 'src3/app/login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
+const appRoutes: Routes = [
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: LoginComponent },
+  { path: '**', component: NopagefoundComponent },
+];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    PagesModule
+  ],
+  exports: [RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
-// Rutas
-import { APP_ROUTES } from './app.routes';
-
-//Módulos
-import { PagesModule } from './pages/pages.module';
-
-
-//Componentes
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { PagesComponent } from './pages/pages.component';
-
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES,
-    PagesModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
